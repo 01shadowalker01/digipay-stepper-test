1be94365eafba0358a65bedbe180d4422fbdb70f
Saeed Bayat <saeedba@systemgroup.net>
2024-01-16 22:06:05 +0330

    feat: create UserInfoFormComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserInfoFormComponent } from './user-info-form/user-info-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: '',
+  imports: [UserInfoFormComponent],
+  template: '<app-user-info-form/>',
 })
 export class AppComponent {}
