659f0b937e7e2f57bae1efc6fb7e777d58c6ecc4
Saeed Bayat <saeedba@systemgroup.net>
2024-01-17 14:15:29 +0330

    fix: fix disabled issue

--- src/app/stepper/directives/stepper-next.directive.ts
+++ src/app/stepper/directives/stepper-next.directive.ts
@@ -1,19 +1,13 @@
-import { Directive, HostBinding, Inject } from '@angular/core';
+import { Directive, Inject } from '@angular/core';
 import { Stepper } from '../interfaces/stepper';
 
 @Directive({
   selector: '[appStepperNext]',
   host: {
     '(click)': 'stepper.next()',
+    '[class.disabled]': '!stepper.isValid()',
   },
 })
 export class StepperNextDirective {
   constructor(@Inject(Stepper) public stepper: Stepper) {}
-
-  @HostBinding('class') get classes() {
-    return {
-      'opacity-50': this.stepper.isValid(),
-      'cursor-not-allowed': this.stepper.isValid(),
-    };
-  }
 }
